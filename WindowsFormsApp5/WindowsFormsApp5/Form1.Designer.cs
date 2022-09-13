04dee7eae3771b2a34e40085632f92f879b5c680
AdamMakoun <113468979+AdamMakoun@users.noreply.github.com>
2022-09-13 20:59:57 +0200

    Add files via upload

---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(370, 200);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(50, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        bool left, right, up , down = false;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+ 
+            if (e.KeyCode == Keys.D)
+            {
+                right = true;
+                down = false;
+                left = false;
+                up = false;
+            }
+            if(e.KeyCode == Keys.W)
+            {
+                right = false;
+                down = false;
+                left = false;
+                up = true;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                right = false;
+                down = true;
+                left = false;
+                up = false;
+            }
+
+            if (e.KeyCode == Keys.A)
+            {
+                right = false;
+                down = false;
+                left = true;
+                up = false;
+            }    
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(right == true)
+            {
+                panel1.Left += 10; //Way easier solution for moving the panel
+
+                //panel1.Location = new Point(panel1.Location.X + 10, panel1.Location.Y);
+
+                
+                if (panel1.Location.X> this.Size.Width)
+                {
+                    panel1.Location = new Point(0, panel1.Location.Y);
+                    
+                }
+            }
+            if(left == true)
+            {
+                panel1.Left -= 10;
+
+                //panel1.Location = new Point(panel1.Location.X - 10, panel1.Location.Y);
+                
+                if (panel1.Location.X < 0)
+                {
+                    panel1.Location = new Point(this.Width - 70, panel1.Location.Y);
+                }
+            }
+
+            if(up == true)
+            {
+                panel1.Top -= 10;
+
+                //panel1.Location = new Point(panel1.Location.X, panel1.Location.Y - 10);
+                if (panel1.Location.Y < 0)
+                {
+                    panel1.Location = new Point(panel1.Location.X, this.Height - 90);
+                }
+            }
+
+            if(down == true)
+            {
+                panel1.Top += 10;
+
+                //panel1.Location = new Point(panel1.Location.X, panel1.Location.Y + 10);
+                if (panel1.Location.Y + 80 > this.Size.Height)
+                {
+                    panel1.Location = new Point(panel1.Location.X, 0);
+                }
+            }
+        }
+    }
+}
